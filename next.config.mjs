724759c1f6375f7a4336ce383392a2a8ac522f3a
Jasson <jeicobfer@gmail.com>
2024-04-17 13:00:14 -0600

    Modific interface

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'res.cloudinary.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: `default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self' data: res.cloudinary.com; connect-src 'self'; media-src 'self'; object-src 'none'; frame-src 'none';`,
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,44 @@
 import React from "react";
-
+import Logo from "../../public/logo.png";
+import Image from "next/image";
+import { Button } from "react-bootstrap";
 function WelcomePage() {
   return (
     <div className="container">
-      <h1 className="mt-5">Bienvenido a SICEM</h1>
-      <p className="lead">El sistema de citas médicas</p>
-      <p>¡Agenda tus citas de manera rápida y sencilla!</p>
-      {/* btn go to login */}
       <div
         className="
           d-flex
+          flex-column
           justify-content-center
           align-items-center
-          mt-3
-        ">
-        <a href="/auth/login" className="btn btn-primary">
+          mt-5
+      ">
+        <Image
+          src={Logo}
+          alt="Logo"
+          width={200}
+          height={200}
+          className="mb-3"
+        />
+        <h1 className="mt-5">¡Bienvenido a la aplicación!</h1>
+      </div>
+
+      <div
+        className="
+          d-flex
+          flex-column
+          justify-content-center
+          align-items-center
+          mt-5
+      ">
+        <p>
+          Esta es una aplicación de ejemplo para mostrar cómo se puede
+          implementar un sistema de autenticación con React y TypeScript.
+        </p>
+
+        <Button variant="primary" href="/auth/login" className="ml-3">
           Iniciar sesión
-        </a>
+        </Button>
       </div>
     </div>
   );
