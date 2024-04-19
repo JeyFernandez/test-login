fb42eaafeeed9399cbe1ed7b691a5f5b88578ee1
Jasson <jeicobfer@gmail.com>
2024-04-19 16:28:38 -0600

    Ui DESING

--- src/app/auth/sign-in/singIn.tsx
+++ src/app/auth/sign-in/singIn.tsx
@@ -2,9 +2,15 @@
 import React, { useState } from "react";
 import fetchData from "@/server/server";
 import UserAuth from "@/interface/authUser.interface";
-import Logo from "../../../../public/img/google.png";
+import Logo_google from "../../../../public/img/google.png";
+import Logo_facebook from "../../../../public/facebook_icon.svg";
+import Doctora from "../../../../public/img/doctora.png";
 import "../../style/login.css";
 import Image from "next/image";
+
+import User from "../../../../public/icons/user.svg";
+import Mail from "../../../../public/icons/mail.svg";
+import Password from "../../../../public/icons/security.svg";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
@@ -41,6 +47,7 @@ export default function SignIn() {
           </p>
           <div className="inputs">
             <div className="form-group">
+              <Image src={User} alt="icon" className="user-icon" />
               <input
                 type="email"
                 className="form-control"
@@ -51,6 +58,7 @@ export default function SignIn() {
               />
             </div>
             <div className="form-group">
+              <Image src={Password} alt="icon" className="user-icon" />
               <input
                 type="password"
                 className="form-control"
@@ -64,12 +72,17 @@ export default function SignIn() {
 
             <div className="other-option">
               <div className="google">
-                <Image src={Logo} alt="Google" className="google-logo" />
+                <Image src={Logo_google} alt="Google" className="google-logo" />
                 <a href="https://accounts.google.com/" type="button">
                   Login with Google
                 </a>
               </div>
               <div className="facebook">
+                <Image
+                  src={Logo_facebook}
+                  alt="Facebook"
+                  className="facebook-logo"
+                />
                 <a href="#" type="button">
                   Login with Facebook
                 </a>
@@ -80,8 +93,20 @@ export default function SignIn() {
       </div>
       <div className="presentation">
         <div className="cards">
-          <h2>SICEM</h2>
-          <p></p>
+          <div>
+            <h2>SICEM</h2>
+            <p>
+              CONSULTORIO
+              <br />
+              MÉDICO
+            </p>
+          </div>
+          <div className="button-card">
+            <button>Sign In</button>
+            <div className="container-img">
+              <Image src={Doctora} alt="doctora" className="doc-image" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
